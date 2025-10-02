bde712f141d697b2c28c2036e4708d6f095b8d05
inocode <fachrezyofficial@gmail.com>
2025-10-03 05:37:05 +0700

    tambah script flowbite

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,8 @@
     <title>DA Esemkasa</title>
     <link rel="icon" href="{{ asset('img/dewan-ambalan.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <script defer src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </head>
 
 <body class="bg-gradient-to-br from-sky-900 to-sky-800 flex items-center justify-center min-h-screen px-4 pb-8 pt-24">
